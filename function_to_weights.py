92d8f58ec0dc396db628ab317887ce911a15452e
eghirardo <edoardoghirardo@gmail.com>
2024-04-18 15:27:54 +0200

    generate graph from file, then add weights from model, compute arrival times instead of weights

---
+++ function_to_weights.py
@@ -0,0 +1,8 @@
+import random
+
+stations = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+
+def model(station, time):
+    # returns the wait time at a station given the time
+    return random.randint(0, 100)
+
